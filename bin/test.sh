410e2509fce8a79e90f8eb38340d9d9149726b46
김정근 <snorlax@ridi.com>
2019-10-15 17:14:35 +0900

    Fix test

--- bin/test.sh
+++ bin/test.sh
@@ -8,8 +8,6 @@ TEST_CMD="aws --version 2>&1 | grep -q 'aws-cli/${AWS_CLI_VERSION}' \
     && echo \"aws-cli version=${AWS_CLI_VERSION}\" \
     && ecs-cli --version 2>&1 | grep -q 'ecs-cli version ${ECS_CLI_VERSION}' \
     && echo \"ecs-cli version=${ECS_CLI_VERSION}\" \
-    && eb --version 2>&1 | grep -q 'EB CLI ${EB_CLI_VERSION}' \
-    && echo \"eb version=${EB_CLI_VERSION}\" \
     && docker-compose --version 2>&1 | grep -q 'docker-compose version ${DOCKER_COMPOSE_VERSION}' \
     && echo \"docker-compose version=${DOCKER_COMPOSE_VERSION}\""
 
